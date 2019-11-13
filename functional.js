8ad92c9a7be24e2fa26f160b54fbdb70121c31e9
Renier Soto <rgabrielsoto@gmail.com>
2019-11-12 22:25:57 -0500

    js reduce

--- functional.js
+++ functional.js
@@ -60,3 +60,47 @@ function newDakota(elements) {
 }
 
 console.log(newDakota(states));
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// sum: Imperative solution
+function imperativeSum(elements) {
+  let total =0;
+  elements.forEach(function(n){
+    total += n;
+  });
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+// sum: Functional solution
+function functionalSum(elements) {
+  return elements.reduce((total,n) => {return total += n;})
+}
+console.log(functionalSum(numbers));
+
+// lengths: Imperative solution
+function imperativeLengths(elements) {
+  let lengths = {};
+  elements.forEach(function(element){
+    lengths[element] = element.length;
+  });
+  return lengths;
+}
+
+console.log(imperativeLengths(states));
+
+// lengths: Functional solution
+function functionalLengths(elements) {
+  return elements.reduce((lengths, element) => {
+                          lengths[element] = element.length;
+                          return lengths;
+  }, {});
+}
+console.log(functionalLengths(states));
+
+function sumProduct(elements) {
+  return elements.reduce((sum, element) => {return sum += element;})
+}
+
+console.log(sumProduct([1,2,3,4,5]));
