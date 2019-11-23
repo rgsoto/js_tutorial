f2edceac7871ffc70cb56477148a928d1fba2d7c
Renier Soto <rgabrielsoto@gmail.com>
2019-11-23 18:44:07 -0500

    Objects and prototypes

--- palindrome.js
+++ palindrome.js
@@ -1,16 +1,32 @@
-// Reverses a string
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise.
+// Adds 'blank' method to all strings (returns true or false)
+String.prototype.blank = function(){
+  return (this.length === 0 || !this.trim());
+}
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
 
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(string);
+  this.processor = function(string) {
+    return this.content.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
 }
 
-function emailParts(email) {
-  let processedContent = email.toLowerCase();
-  return processedContent + "@example.com";
-}
+// Adds a "last" method to all Arrays
+Array.prototype.last = function() {
+  return this[this.length - 1];
+};
